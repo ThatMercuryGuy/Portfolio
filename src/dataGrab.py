17b744cfe66be03645d81eca643e2f2dc95e54f9
ThatMercuryGuy <sumedhracing@gmail.com>
2024-11-02 18:31:21 +0530

    port dataframe binary to JSON

--- src/dataGrab.py
+++ src/dataGrab.py
@@ -1,20 +1,23 @@
 from yahoo_fin.stock_info import get_data
 import pickle
+import json
 import os
+import pandas
 
 #Load ticker list from obj/tickers.dat
 script_dir = os.path.dirname(__file__)
 with open (f'{script_dir}\\..\\obj\\tickers.dat', 'rb') as f:
     nifty_50_symbols = pickle.load (f)
 
-data = {}
+data = list ()
 
 #API Call to retrieve historical data
 for ticker in nifty_50_symbols:
-    data[ticker] = get_data(ticker, start_date='01/01/2024', end_date='10/20/2024', interval='1wk')
+    data.append(get_data(ticker, start_date='01/01/2024', end_date='10/20/2024', interval='1wk').to_json ())
 
 
-#Pickle DataFrame (dict) to obj/dataframe.dat
-with open(f'{script_dir}\\..\\obj\\dataframe.dat', 'wb') as f:
-    pickle.dump(data, f)
+#Serialize DataFrame (dict) to obj/dataframe.json
+with open(f'{script_dir}\\..\\obj\\dataframe.json', 'w') as f:
+    print((type(data)))
+    json.dump(data, f)
 
