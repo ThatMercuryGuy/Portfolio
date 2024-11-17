8fd7d726be4e9b6055885f7345d72df39a376445
ThatMercuryGuy <sumedhracing@gmail.com>
2024-11-17 22:00:44 +0530

    Serialize to CSV to perform Julia transfer

--- src/dataGrab.py
+++ src/dataGrab.py
@@ -3,6 +3,7 @@ import pickle
 import json
 import os
 import pandas
+import csv
 
 #Load ticker list from obj/tickers.dat
 script_dir = os.path.dirname(__file__)
@@ -13,11 +14,19 @@ data = list ()
 
 #API Call to retrieve historical data
 for ticker in nifty_50_symbols:
-    data.append(get_data(ticker, start_date='01/01/2024', end_date='10/20/2024', interval='1wk').to_json ())
+    data.append(get_data(ticker, start_date='01/01/2024', end_date='10/20/2024', interval='1wk'))
 
 
+df = pandas.DataFrame ()
+for i in range (len (data)):
+    df = pandas.concat ([df, data[i]])
+
+df.to_csv (f'{script_dir}\\..\\obj\\dataframe.csv')
+
+'''with open(f'{script_dir}\\..\\obj\\dataframe.csv', 'w') as f:
+    w = csv.writer (f)
+    w.writerows(data)'''
 #Serialize DataFrame (dict) to obj/dataframe.json
-with open(f'{script_dir}\\..\\obj\\dataframe.json', 'w') as f:
+'''with open(f'{script_dir}\\..\\obj\\dataframe.json', 'w') as f:
     print((type(data)))
-    json.dump(data, f)
-
+    json.dump(data, f)'''
