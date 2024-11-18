73d048280250810fe0999c1e958ff910a04824b8
ThatMercuryGuy <sumedhracing@gmail.com>
2024-11-18 23:18:58 +0530

    Changes: 1. formatted CSV correctly, 2. created Markowitz model

--- main.jl
+++ main.jl
@@ -2,8 +2,27 @@ using CSV
 using DataFrames
 using Statistics
 
-csv_file = CSV.File("temp.csv")
+csv_file = CSV.File("fmtemp.csv")
 df = DataFrame(csv_file)
 
 
 names(df)
+returns = diff(Matrix(df); dims = 1) ./ Matrix(df[1:end-1, :])
+
+r = vec(Statistics.mean(returns; dims = 1))
+Q = Statistics.cov(returns)
+
+using JuMP
+import Ipopt
+model = Model(Ipopt.Optimizer)
+
+@variable(model, x[1:50] >= 0)
+@objective(model, Min, x' * Q * x)
+@constraint(model, sum(x) <= 100)
+@constraint(model, r' * x >= 5)
+optimize!(model)
+
+@assert is_solved_and_feasible(model)
+solution_summary(model)
+
+value.(x)
