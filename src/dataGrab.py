5f0b31f5872b31eb558974efd9450f08ad9525f6
ThatMercuryGuy <sumedhracing@gmail.com>
2024-11-19 17:35:10 +0530

    Fixed formatting and pull monthly data

--- src/dataGrab.py
+++ src/dataGrab.py
@@ -1,8 +1,6 @@
 from yahoo_fin.stock_info import get_data
 import pickle
-import json
 import os
-import pandas
 import csv
 
 #Load ticker list from obj/tickers.dat
@@ -14,11 +12,16 @@ data = list ()
 
 #API Call to retrieve historical data
 for ticker in nifty_50_symbols:
-    data.append(get_data(ticker, start_date='01/01/2024', end_date='10/20/2024', interval='1wk'))
+    data.append(get_data(ticker, start_date='01/01/2024', end_date='11/19/2024', interval='1mo'))
 
 #Add all data to a single CSV file
-df = pandas.DataFrame ()
-for i in range (len (data)):
-    df = pandas.concat ([df, data[i]])
+with open (f'{script_dir}\\..\\obj\\dataframe_monthly.csv', 'w', newline = '') as f:
+    w = csv.writer (f)
+    w.writerow (nifty_50_symbols)
+    for i in range (len (data[0])): #WEEKS
+        arr = []
+        for j in range (len (data)): #SHARES
+            arr.append(data[j]["close"][i])
+        w.writerow (arr)
 
-df.to_csv (f'{script_dir}\\..\\obj\\dataframe.csv')
+
