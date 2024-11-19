075f51a8e02c0e34274b48a00888556fd4e1176c
ThatMercuryGuy <sumedhracing@gmail.com>
2024-11-19 19:35:25 +0530

    Markowitz Optimization Complete; Data Rep remaining

--- main.jl
+++ main.jl
@@ -2,7 +2,7 @@ using CSV
 using DataFrames
 using Statistics
 
-csv_file = CSV.File("fmtemp.csv")
+csv_file = CSV.File("obj/dataframe_monthly.csv")
 df = DataFrame(csv_file)
 
 
@@ -18,13 +18,15 @@ import Ipopt
 
 model = Model(() -> MOA.Optimizer(Ipopt.Optimizer))
 set_optimizer_attribute(model, MOA.Algorithm(), MOA.EpsilonConstraint())
-set_optimizer_attribute(model, MOA.SolutionLimit(), 50)
-set_silent(model)
+set_optimizer_attribute(model, MOA.SolutionLimit(), 20)
+#set_silent(model)
 
 @variable(model, x[1:50] >= 0)
-@constraint(model, sum(x) <= 1000)
+@constraint(model, sum(x) <= 100)
 @expression(model, variance, x' * Q * x)
 @expression(model, expected_return, r' * x)
+
+#=@variable(model, bought[1:50], )=#
 # We want to minimize variance and maximize expected return, but we must pick
 # a single objective sense `Min`, and negate any `Max` objectives:
 @objective(model, Min, [variance, -expected_return])
@@ -32,18 +34,13 @@ optimize!(model)
 @assert termination_status(model) == OPTIMAL
 solution_summary(model)
 
-scalar_return = value(r' * x)
-scalar_variance = value(x' * Q * x)
-
 using Plots
 using StatsPlots
 
 objective_space = Plots.hline(
-    [scalar_return];
-    label = "Single-objective solution",
-    linecolor = :red,
+
 )
-Plots.vline!(objective_space, [scalar_variance]; label = "", linecolor = :red)
+
 Plots.scatter!(
     objective_space,
     [value(variance; result = i) for i in 1:result_count(model)],
@@ -58,13 +55,13 @@ Plots.scatter!(
 )
 for i in 1:result_count(model)
     y = objective_value(model; result = i)
-    Plots.annotate!(objective_space, y[1], -y[2], (i, 3))
+    Plots.annotate!(y[1], -y[2], (i, 3))
 end
 
 decision_space = StatsPlots.groupedbar(
     vcat([value.(x; result = i)' for i in 1:result_count(model)]...);
     bar_position = :stack,
-    label = names(df),
+
     xlabel = "Solution #",
     ylabel = "Investment (\$)",
     title = "Decision space",
